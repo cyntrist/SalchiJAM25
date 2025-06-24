25fd780e861029d660e991a076e10d50ecf6a7bb
Cynthia Tristán <cyntrist@ucm.es>
2025-06-24 18:02:16 +0200

    Update Fade.gd

--- project/scripts/flujo/Fade.gd
+++ project/scripts/flujo/Fade.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 func transition():
-	print("DENTRO FADE IN")
+	#print("DENTRO FADE IN")
 	$AnimationPlayer.play("fade_in")
 	# Global.gms.sfx.stream = load("res://Sounds/fade/683096__florianreichelt__woosh.mp3")
 	# Global.gms.sfx.play()
@@ -16,9 +16,9 @@ func transition():
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade_in":
-		print("FUERA FADE IN")
+		#print("FUERA FADE IN")
 		Global.on_transition_end.emit()
 		$AnimationPlayer.play("fade_out")
-	elif anim_name == "fade_out":
-		print("FUERA FADE OUT")
+	#elif anim_name == "fade_out":
+		#print("FUERA FADE OUT")
 		
