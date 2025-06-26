34382fbe51327bc8570688af43bb393ae3b6ff5d
Andrés García Navarro <andrga37@ucm.es>
2025-06-26 17:00:47 +0200

    hola colliders puestos

--- project/scripts/image_comparator.gd
+++ project/scripts/image_comparator.gd
@@ -1,13 +1,17 @@
 extends Node
+const ICON = preload("res://icon.png")
+func _ready() -> void:
+	var image = Image.load_from_file("res://icon.png")
+	var similitude = comparate(image, image)
+	print(similitude * 100, "%")
 
 func comparate(img1: Image, img2: Image) -> float:
-	return 0
-
-func to_black_white(img: Image) -> Image:
-	var imgSize = img.get_size()
-	var newImage := img
+	var imgSize = img1.get_size()
+	var newImage := img1
+	var equalPixels = 0
 	newImage.resize(imgSize.x, imgSize.y)
-	for i in range(img.get_size().x):
-		for j in range(img.get_size().y):
-			pass
-	return newImage
+	for i in range(imgSize.x):
+		for j in range(imgSize.y):
+			if img1.get_pixel(i,j) == img2.get_pixel(i,j):
+				equalPixels += 1
+	return equalPixels / (imgSize.x * imgSize.y)
