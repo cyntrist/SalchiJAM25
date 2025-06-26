4236bf52a89093477d3dd2f8db2bb0f970c3efb1
Andrés García Navarro <andrga37@ucm.es>
2025-06-26 16:05:10 +0200

    HOLA HOLA

--- project/scripts/image_comparator.gd
+++ project/scripts/image_comparator.gd
@@ -1,1 +1,13 @@
 extends Node
+
+func comparate(img1: Image, img2: Image) -> float:
+	return 0
+
+func to_black_white(img: Image) -> Image:
+	var imgSize = img.get_size()
+	var newImage := img
+	newImage.resize(imgSize.x, imgSize.y)
+	for i in range(img.get_size().x):
+		for j in range(img.get_size().y):
+			pass
+	return newImage
