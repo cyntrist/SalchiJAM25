04fb830bcd67e1c704efcc7f79cc03f2078355a0
Cynthia Tristán <cyntrist@ucm.es>
2025-06-26 18:09:01 +0200

    Update silhouette_button.gd

--- project/scripts/silhouette_button.gd
+++ project/scripts/silhouette_button.gd
@@ -1,22 +1,20 @@
 extends Button
 
 func _ready() -> void:
+	Global.nextLevel.connect(_next_stage)
 	pass
 
 func _pressed() -> void:
 	#print("BOTON PULSADO");
-	
+	Global.checkLevel.emit(); # comprobar si se acerca
+	pass
 
+
+func _next_stage():
 	if (Global.stage == Global.maxStages):
 		Global.change_scene(Global.Scenes.CREDITS, 0.5);
 		pass;
-	
-	# AQUÍ GESTION DE SI PASA AL SIGUIENTE SI ES SUFICIENTEMENTE ACERTADO
 	else:
 		Global.next_stage();
-		Global.checkLevel.emit();
 		Global.change_scene(Global.Scenes.GAME);
-
-	# si no pues algun tipo de efecto 
-	# ...
-	pass
+	pass;
